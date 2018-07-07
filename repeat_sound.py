4ddf3ba1105fde06da8a9b36cf6ad57ceb0d8deb
Harold Pimentel <haroldpimentel@gmail.com>
2018-07-07 11:11:40 -0700

    Timer -> cron #talon

--- repeat_sound.py
+++ repeat_sound.py
@@ -1,24 +1,22 @@
-from talon.voice import Context, Rep, RepPhrase, talon
-from user.utils import parse_words_as_integer
+from talon.voice import Context, Rep, talon
 from talon.audio import noise
-
-from threading import Timer, Lock
+from talon import cron
 
 context = Context('repeat_sound')
 
 
 class Repeater:
-    def __init__(self, initial_delay = 0.5, repeat_delay = 0.1):
+    def __init__(self, initial_delay = '500ms', repeat_delay = '100ms'):
         self.initial_delay = initial_delay
         self.repeat_delay = repeat_delay
         noise.register('noise', self.on_noise)
-        self.lock = Lock()
-        self.running = False
-        self.timer = None
+        self.job = None
 
 
     def disable(self):
         noise.unregister('noise', self.on_noise)
+        if self.job:
+            cron.cancel(self.job)
 
 
     def enable(self):
@@ -26,36 +24,23 @@ class Repeater:
 
 
     def on_noise(self, noise):
-        if noise == 'hiss_start':
-            with self.lock:
-                if self.running:
-                    return
+        if noise == 'hiss_start' and talon.enabled:
+            if self.job is None:
+                self.job = cron.after(self.initial_delay, self.repeat)
                 print('HISS START')
-                self.running = True
-                self.timer = Timer(self.initial_delay, self.repeat)
-            if self.timer is not None:
-                self.timer.start()
-        elif noise == 'hiss_end':
-            with self.lock:
-                if not self.running:
-                    return
-                self.timer.cancel()
-                self.timer = None
-                self.running = False
+        elif noise == 'hiss_end' and self.job:
             print('HISS STOP')
+            cron.cancel(self.job)
+            self.job = None
 
 
     def repeat(self):
         repeater = Rep(1)
         repeater.ctx = talon
         repeater(None)
-        with self.lock:
-            if not self.running:
-                return
+        if self.job:
+            self.job = cron.after(self.repeat_delay, self.repeat)
             print('REPEAT')
-            self.timer = Timer(self.repeat_delay, self.repeat)
-        if self.timer is not None:
-            self.timer.start()
 
 repeater = Repeater()
 
